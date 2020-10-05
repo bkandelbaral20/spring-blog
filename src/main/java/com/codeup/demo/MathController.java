d55299171d1896f77f9f477c23591e0900dd0bc5
Binjita Kandel Baral <binjita.baral@gmail.com>
2020-10-05 12:19:19 -0500

    adding dependency for view

--- src/main/java/com/codeup/demo/MathController.java
+++ src/main/java/com/codeup/demo/MathController.java
@@ -17,14 +17,14 @@ class MathController {
     @RequestMapping(path = "/subtract/{number1}/from/{number2}", method = RequestMethod.GET)
     @ResponseBody
     public String subtract(@PathVariable int number1, @PathVariable int number2) {
-        return number1 + " subtract " + (number2) + " is  = " + (number1-number2) ;
+        return number1 + " subtract from  " + (number2) + " is  = " + (number1-number2) ;
     }
 
 
     @RequestMapping(path = "/multiply/{number1}/and/{number2}", method = RequestMethod.GET)
     @ResponseBody
     public String multiply(@PathVariable int number1, @PathVariable int number2) {
-        return number1 + " multiplied  by " + (number2) + " is  = " + (number1*number2) ;
+        return number1 + " multiplied by " + (number2) + " is  = " + (number1*number2) ;
     }
 
     @RequestMapping(path = "/divide/{number1}/by/{number2}", method = RequestMethod.GET)
