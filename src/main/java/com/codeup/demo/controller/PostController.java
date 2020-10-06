68cc65eb467e2bc435d4a0d76b48aeb80a1bcedb
Binjita Kandel Baral <binjita.baral@gmail.com>
2020-10-06 10:48:51 -0500

    working on posts

--- src/main/java/com/codeup/demo/controller/PostController.java
+++ src/main/java/com/codeup/demo/controller/PostController.java
@@ -1,18 +1,18 @@
-//package com.codeup.demo.controller;
-//
-//import com.codeup.demo.models.Post;
-//import org.springframework.stereotype.Controller;
-//import org.springframework.ui.Model;
-//import org.springframework.web.bind.annotation.*;
-//
-//@Controller
-//public class PostController {
-//
-////    @RequestMapping(path = "/posts", method = RequestMethod.GET)
-////    @ResponseBody
-////    public String post1() {
-////        return "posts index page";
-////    }
+package com.codeup.demo.controller;
+
+import com.codeup.demo.models.Post;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+public class PostController {
+
+    @RequestMapping(path = "/posts", method = RequestMethod.GET)
+    @ResponseBody
+    public String post1() {
+        return "posts index page";
+    }
 //    @GetMapping("/posts/{id}")
 //    public String showAd(@PathVariable long id, Model model) {
 //        // get by id
@@ -20,23 +20,24 @@
 //        model.addAttribute("post", po);
 //        return("posts/po");
 //    }
-//
-//    @RequestMapping (path  = "/posts/{id}", method = RequestMethod.GET)
-//    @ResponseBody
-//    public String post2(@PathVariable long id) {
-//        return "view an individual post";
-//    }
-//
-//    @RequestMapping(path = "/posts/create", method = RequestMethod.GET)
-//    @ResponseBody
-//    public String post3() {
-//        return "view the form for creating a post";
-//    }
-//
-//    @RequestMapping(path = "/posts/create", method = RequestMethod.POST)
-//    @ResponseBody
-//    public String post4() {
-//        return "create a new post";
-//    }
-//
-//}
+
+    @RequestMapping (path  = "/posts/{id}", method = RequestMethod.GET)
+    public String showPost(@PathVariable long id, Model model) {
+        Post po = new Post("Single post Title", "Description for a single post.");
+        model.addAttribute("post", po);
+        return("posts/{id}");
+    }
+
+    @RequestMapping(path = "/posts/create", method = RequestMethod.GET)
+    @ResponseBody
+    public String post3() {
+        return "view the form for creating a post";
+    }
+
+    @RequestMapping(path = "/posts/create", method = RequestMethod.POST)
+    @ResponseBody
+    public String post4() {
+        return "create a new post";
+    }
+
+}
