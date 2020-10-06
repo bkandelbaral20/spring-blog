a803e9897f74dd9f6c506d890b02b4f824d198b0
Binjita Kandel Baral <binjita.baral@gmail.com>
2020-10-06 09:52:20 -0500

    done with rice-dice

--- src/main/java/com/codeup/demo/controller/PostController.java
+++ src/main/java/com/codeup/demo/controller/PostController.java
@@ -1,36 +1,42 @@
-package com.codeup.demo.controller;
-
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
-
-@Controller
-public class PostController {
-
-    @RequestMapping(path = "/posts", method = RequestMethod.GET)
-    @ResponseBody
-    public String post1() {
-        return "posts index page";
-    }
-
-    @RequestMapping (path  = "/posts/{id}", method = RequestMethod.GET)
-    @ResponseBody
-    public String post2(@PathVariable long id) {
-        return "view an individual post";
-    }
-
-    @RequestMapping(path = "/posts/create", method = RequestMethod.GET)
-    @ResponseBody
-    public String post3() {
-        return "view the form for creating a post";
-    }
-
-    @RequestMapping(path = "/posts/create", method = RequestMethod.POST)
-    @ResponseBody
-    public String post4() {
-        return "create a new post";
-    }
-
-}
+//package com.codeup.demo.controller;
+//
+//import com.codeup.demo.models.Post;
+//import org.springframework.stereotype.Controller;
+//import org.springframework.ui.Model;
+//import org.springframework.web.bind.annotation.*;
+//
+//@Controller
+//public class PostController {
+//
+////    @RequestMapping(path = "/posts", method = RequestMethod.GET)
+////    @ResponseBody
+////    public String post1() {
+////        return "posts index page";
+////    }
+//    @GetMapping("/posts/{id}")
+//    public String showAd(@PathVariable long id, Model model) {
+//        // get by id
+//        Post po = new Post("Single ad Title", "Description for a single ad.");
+//        model.addAttribute("post", po);
+//        return("posts/po");
+//    }
+//
+//    @RequestMapping (path  = "/posts/{id}", method = RequestMethod.GET)
+//    @ResponseBody
+//    public String post2(@PathVariable long id) {
+//        return "view an individual post";
+//    }
+//
+//    @RequestMapping(path = "/posts/create", method = RequestMethod.GET)
+//    @ResponseBody
+//    public String post3() {
+//        return "view the form for creating a post";
+//    }
+//
+//    @RequestMapping(path = "/posts/create", method = RequestMethod.POST)
+//    @ResponseBody
+//    public String post4() {
+//        return "create a new post";
+//    }
+//
+//}
--- src/main/java/com/codeup/demo/controller/RollDiceController.java
+++ src/main/java/com/codeup/demo/controller/RollDiceController.java
@@ -1,6 +1,8 @@
 package com.codeup.demo.controller;
 
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -9,21 +11,27 @@ import org.springframework.web.bind.annotation.ResponseBody;
 // they guessed the correct number.
 @Controller
 class RollDiceController {
-
-    @GetMapping("/roll-dice/n")
-    @ResponseBody
-    public int rollDice(@PathVariable int n){
-        return  n;
+    @GetMapping("/roll-dice")
+    public String shoRollDice() {
+        return "roll-dice";
     }
-    public static int randomNumbers( String args[] ) {
+
+    @GetMapping("/roll-dice/{n}")
+    public String rollDice(@PathVariable int n, Model model) {
         int min = 1;
         int max = 6;
-        int random_int;
+        int random_int = (int) (Math.random() * (max - min + 1) + min);
 
-        //Generate random int value from 1 to 6
-        System.out.println("Random value in int from "+min+" to "+max+ ":");
-        return random_int = (int)(Math.random() * (max - min + 1) + min);
+        String alert = "Sorry! you did not guess the right number";
+
+        if (n == random_int) {
+            alert = "Congratulations! you win the game ";
         }
 
+//------------- passing data to the view
+        model.addAttribute("random", random_int);
+        model.addAttribute("rightGuess", alert);
+        return "roll-dice";
     }
+}
 
