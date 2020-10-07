7499e88521b29a48b5879edbc49eca27c599d54e
Binjita Kandel Baral <binjita.baral@gmail.com>
2020-10-07 10:23:44 -0500

    adding interface

--- src/main/java/com/codeup/demo/models/Post.java
+++ src/main/java/com/codeup/demo/models/Post.java
@@ -1,9 +1,17 @@
 package com.codeup.demo.models;
+import javax.persistence.*;
 
+@Entity
+@Table(name="blogs")
 public class Post {
-
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
+
+    @Column(nullable = false, length = 100)
     private String title;
+
+    @Column(nullable = false)
     private String body;
 
     public Post(){}
