163da843147c51650b5d274cc5d3728b1e6997c9
Binjita Kandel Baral <binjita.baral@gmail.com>
2020-10-06 19:01:41 -0500

    adding different files

--- src/main/java/com/codeup/demo/controller/PostController.java
+++ src/main/java/com/codeup/demo/controller/PostController.java
@@ -5,29 +5,18 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Controller
 public class PostController {
 
+//    -------------servlets exercises
     @RequestMapping(path = "/posts", method = RequestMethod.GET)
     @ResponseBody
     public String post1() {
         return "posts index page";
     }
-//    @GetMapping("/posts/{id}")
-//    public String showAd(@PathVariable long id, Model model) {
-//        // get by id
-//        Post po = new Post("Single ad Title", "Description for a single ad.");
-//        model.addAttribute("post", po);
-//        return("posts/po");
-//    }
-
-    @RequestMapping (path  = "/posts/{id}", method = RequestMethod.GET)
-    public String showPost(@PathVariable long id, Model model) {
-        Post po = new Post("Single post Title", "Description for a single post.");
-        model.addAttribute("post", po);
-        return("posts/{id}");
-    }
-
     @RequestMapping(path = "/posts/create", method = RequestMethod.GET)
     @ResponseBody
     public String post3() {
@@ -40,4 +29,21 @@ public class PostController {
         return "create a new post";
     }
 
+// ------------View exercises
+
+    @GetMapping("/posts/{id}")
+    public String showOnePost(@PathVariable long id, Model model) {
+        Post post = new Post(0, "Single Post", "This is the boday for the single post");
+        model.addAttribute("post", post);
+        return "posts/show";
+    }
+
+    @RequestMapping(path = "/posts", method = RequestMethod.GET)
+    public String showAllPosts(Model model) {
+        List<Post> postList = new ArrayList<>();
+        postList.add(new Post(0, "First Post", "This is the first Post"));
+        postList.add(new Post(0, "Second Post", "This is the second Post"));
+        model.addAttribute("posts", postList);
+        return "posts/index";
+    }
 }
