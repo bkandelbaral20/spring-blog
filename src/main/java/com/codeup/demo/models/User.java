c22df50d80ad93632999615fe6f7014c8309d9bc
Binjita Kandel Baral <binjita.baral@gmail.com>
2020-10-08 21:01:30 -0500

    created user tables on the database

--- src/main/java/com/codeup/demo/models/User.java
+++ src/main/java/com/codeup/demo/models/User.java
@@ -1,9 +1,21 @@
 package com.codeup.demo.models;
 
+import javax.persistence.*;
+
+@Entity
+@Table(name="users")
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
+
+    @Column(nullable = false, length = 100)
     private String username;
+
+    @Column(nullable = false, length = 100)
     private String email;
+
+    @Column(nullable = false, length = 100)
     private String password;
 
     public User(long id, String username, String email, String password) {
---
+++ src/main/java/com/codeup/demo/repository/UserRepository.java
@@ -0,0 +1,4 @@
+package com.codeup.demo.repository;
+
+public interface UserRepository {
+}
